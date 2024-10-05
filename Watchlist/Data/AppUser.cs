91c17dafc98c9bacf7b86da018db24f114a890cd
Rashy-hub <yagoubi.r@gmail.com>
2024-10-05 16:48:56 +0200

    Add AppUser entity inheriting from IdentityUser with additional properties

---
+++ Watchlist/Data/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Watchlist.Data
+{
+    public class AppUser: IdentityUser
+    {
+
+        public AppUser() : base()
+        {
+            this.MovieList = new HashSet<MovieUser>();
+        }
+
+        public string firstname { get; set; }
+        public virtual ICollection<MovieUser> Movies { get; set; }
+        public HashSet<MovieUser> MovieList { get; private set; }
+    }
+}
