c7a0d9c7475d58e6297883da1565b94768fa4eea
Rashy-hub <yagoubi.r@gmail.com>
2024-10-05 16:49:09 +0200

    Add MovieUser entity to represent many-to-many relationship between AppUser and Movie

---
+++ Watchlist/Data/MovieUser.cs
@@ -0,0 +1,13 @@
+﻿namespace Watchlist.Data
+{
+    public class MovieUser
+    {
+        public string IdUser { get; set; }
+        public int IdMovie { get; set; }
+        public int Note { get; set; }
+        public bool hasSeen { get; set; }
+
+        public virtual AppUser User { get; set; }
+        public virtual Movie Film { get; set; }
+    }
+}
