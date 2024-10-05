19a3ccd30d289afccebcca8185f2d8a19a373a8e
Rashy-hub <yagoubi.r@gmail.com>
2024-10-05 22:52:08 +0200

    Add file : Movie view model

---
+++ Watchlist/Models/MovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Watchlist.Models
+{
+    public class MovieViewModel
+    {       
+         public int IdMovie { get; set; }
+         public string Title { get; set; }
+         public int Year { get; set; }
+         public bool isInTheList { get; set; }
+         public bool hasSeen { get; set; }
+         public int? Rating { get; set; }
+       
+    }
+}
