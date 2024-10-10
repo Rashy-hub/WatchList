383c401a5a8b8c60a09f01481617651c6d4932b8
Rashy-hub <yagoubi.r@gmail.com>
2024-10-10 19:43:23 +0200

    Add Services EmailSender

---
+++ Watchlist/Services/EmailSender.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Threading.Tasks;
+
+public class EmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        // Implémentez la logique d'envoi d'e-mail ici
+        return Task.CompletedTask;
+    }
+}
